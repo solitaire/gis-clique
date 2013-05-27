c29a89574b92e1f1390b7cd2f9b9f03150c74047
Adam <stelmaszczykadam@gmail.com>
2013-05-27 14:33:49 +0200

    Opis zawartości katalogów, instrukcja obsługi, drobna modyfikacja run.sh

--- java-workspace/run.sh
+++ java-workspace/run.sh
@@ -4,8 +4,14 @@ for file in ../tests/*in.txt
 do
     echo -n "$file: "
     answer=$(/usr/bin/time -f "%Us" java -cp bin main/Main < $file)
-    if [ $(grep -c "$answer" "${file%_*}_out.txt") -eq 0 ]
+    outfile="${file%_*}_out.txt"
+    if [ ! -e $outfile ]
     then
-    	echo "Fail. This is not maximum clique: $answer"
-   	fi 
+    	echo "No $outfile file to compare. Maximum clique: $answer"
+    else
+		if [ $(grep -c "$answer" $outfile) -eq 0 ]
+		then
+			echo "Fail. This is not maximum clique: $answer"
+	   	fi 
+   	fi
 done
